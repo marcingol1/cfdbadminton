efe50ba256c13e0c4b8f882f4f7471dceb3c740e
Marcin Gol <golmarcin@gmail.com>
2018-11-01 00:08:50 +0100

    add test :any component :joy:

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import TopBarComponent from './top-bar/top-bar-component';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <TopBarComponent />
+        hello!
       </div>
     );
   }
