f5e7aa80299291e3ab9ce61d2e333e1dd71407f8
Marcin Gol <golmarcin@gmail.com>
2018-11-01 14:45:35 +0100

    extract view component, refactor top bar component -> HOC :fire:

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import TopBarComponent from './top-bar/top-bar-component';
 import ThemeHoc from './theme/theme-hoc';
+import DashboardComponent from './dashboard/dashboard';
+
+const appTitle = 'Crazy freakin\' deadly badminton!';
 
 function MyApp() {
   return (
     <ThemeHoc>
       <CssBaseline>
-        <TopBarComponent />
+        <TopBarComponent title={appTitle}>
+          <DashboardComponent />
+        </TopBarComponent>
       </CssBaseline>
     </ThemeHoc>
   );
