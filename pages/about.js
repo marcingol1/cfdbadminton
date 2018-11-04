33a9f79187bb7a363b5f23f5c4800f31381b9f5d
Marcin Gol <golmarcin@gmail.com>
2018-11-04 11:11:46 +0100

    basic micro setup for next.js

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const about = () => {
+  return (
+    <div>
+      'Hello world'
+    </div>
+  )
+}
+
+export default about;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 
 import TopBarComponent from './../components/top-bar/top-bar-component';
