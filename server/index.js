c05cab450770c7c34f742ef328e2c3c4e9547951
Marcin Gol <golmarcin@gmail.com>
2018-11-12 18:31:19 +0100

    apollo server mocking of data :fire:

--- server/index.js
+++ server/index.js
@@ -3,34 +3,8 @@ import path from 'path';
 import cors from 'cors';
 import { ApolloServer, gql } from 'apollo-server-express';
 
-const { prisma } = require('./prisma');
+import { prisma } from './prisma';
 
-const regions = ['Europe', 'Asia', 'NorthAmerica', 'SouthAmerica'];
-const gameTypes = ['Deathmatch', 'CaptureTheFlag', 'Arena'];
-
-function generateRandomListElement() {
-  return {
-    id: `asod-123-132`,
-    title: 'Some random title',
-    playersOnline: Math.round(Math.random() * 120),
-    region: regions[Math.floor(Math.random() * (regions.length))],
-    match: {
-      score: Math.round(Math.random() * 100),
-      gameType: gameTypes[Math.floor(Math.random() * (gameTypes.length))],
-      players: [
-        {
-          id: 'asda-jakies-id',
-          online: true,
-          gamesHistory: [{
-            score: Math.round(Math.random() * 100),
-            gameType: gameTypes[Math.floor(Math.random() * (gameTypes.length))],
-          }]
-        }
-      ]
-    }
-  };
-}
-const servers = (new Array(15)).fill({}).map(generateRandomListElement);
 const typeDefs = gql`
   enum GameType {
     Deathmatch
@@ -69,12 +43,16 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    servers: () => servers,
+    servers: () => {},
   },
 };
 
 const buildPath = '../build';
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  mocks: true,
+  // resolvers
+});
 
 const app = express();
 app.use(cors());
@@ -91,21 +69,6 @@ app.get('/ping', function (req, res) {
 server.applyMiddleware({ app });
 const port = process.env.PORT || 8080;
 
-// A `main` function so that we can use async/await
-async function main() {
-
-  // Create a new user called `Alice`
-  const newUser = await prisma.createUser({ name: 'Alice' })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
-
-  // Read all users from the database and print them to the console
-  const allUsers = await prisma.users()
-  console.log(allUsers)
-}
-
-main().catch(e => console.error(e))
-
-app.listen(port, () => {
-  console.log(`🚀 GraphQL endpoint ready at http://localhost:8080${server.graphqlPath}`);
-  console.log('🚀 Default API endpoint hosted at http://localhost:3000');
+app.listen({ port }, () => {
+  console.log(`\n🚀 Server ready at: http://localhost:${port}${server.graphqlPath}\n`);
 });
