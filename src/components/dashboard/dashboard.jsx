27207c990fb6c0dbbafbdaaa3a6fa45eaee22171
Marcin Gol <golmarcin@gmail.com>
2018-11-13 22:03:13 +0100

    graphql front-end query!

--- src/components/dashboard/dashboard.jsx
+++ src/components/dashboard/dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core';
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
 
 import ServerList from './../server-list/server-list';
 
@@ -13,6 +15,30 @@ const styles = (theme) => ({
     justifyContent: 'flex-end',
   }
 });
+
+const client = new ApolloClient();
+console.log(client);
+
+client
+  .query({    query: gql`
+    {
+    servers {
+        id
+        region
+        match {
+          score
+          gameType,
+          players {
+            gamesHistory{
+              score,
+              gameType
+            }
+          }
+        }
+      }
+    }
+    `
+  }).then(data => console.log('data', data));
 
 export class DashboardComponent extends Component {
   static propTypes = {
