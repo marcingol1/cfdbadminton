c8ed806b8f05b7b9e57568a2f9c057c4ec140424
Marcin Gol <golmarcin@gmail.com>
2018-11-18 20:47:54 +0100

    remove all front-end files

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # CFDBadminton
-# API hosted by Heroku via Prisma Cloud
+API hosted by Heroku via Prisma Cloud
 
 # If you want to run this locally:
-1.  run:  `npm run dev`
+
+`npm run dev`
--- src/services/hello-service.js
+++
@@ -1,9 +0,0 @@
-
-
-export const fetchHello = async () => {
-  console.log('starting fetch of data...');
-  const res = await fetch('/ping');
-  const data = await res.json();
-  console.log('final data is: ', data);
-  return data;
-}
